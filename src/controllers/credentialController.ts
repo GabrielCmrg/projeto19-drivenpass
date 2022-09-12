36240c57343254d5d53812a7d30324f5ff247638
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-09-12 14:37:28 -0300

    feat: add controllers for credential searching

--- src/controllers/credentialController.ts
+++ src/controllers/credentialController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 import {
+  Credential,
   CredentialCreationData,
   CredentialData,
   CredentialRequestData
@@ -18,4 +19,23 @@ export async function createCredential(
   const createdCredential: CredentialData = await credentialService
     .makeNewCredential(credentialToCreate);
   return res.status(201).json(createdCredential);
+}
+
+export async function searchUserCredentials(
+  req: Request,
+  res: Response<any, LocalsType>
+): Promise<Response> {
+  const userId: number = res.locals.userId;
+  const credentials: Credential[] = await credentialService.retrieveUserCredentials(userId);
+  return res.status(200).json(credentials);
+}
+
+export async function searchCredential(
+  req: Request<{ credentialId: string }>,
+  res: Response<any, LocalsType>
+): Promise<Response> {
+  const credentialId: number = Number(req.params.credentialId);
+  const userId: number = res.locals.userId;
+  const credential: Credential = await credentialService.retrieveCredential(credentialId, userId);
+  return res.status(200).json(credential);
 }
