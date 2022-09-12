f128b29a736daa74d7813548e8ac2fffdc2a7e41
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-09-12 17:47:08 -0300

    feat: create note endpoints

--- src/schemas/noteSchemas.ts
+++ src/schemas/noteSchemas.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 import { NoteRequestData } from '../types/noteTypes';
 
-export const credentialRequestSchema: Joi.ObjectSchema<NoteRequestData> = Joi
+export const noteRequestSchema: Joi.ObjectSchema<NoteRequestData> = Joi
   .object<NoteRequestData, true>({
     title: Joi.string().trim().max(50).required(),
     body: Joi.string().trim().max(1000).required(),
