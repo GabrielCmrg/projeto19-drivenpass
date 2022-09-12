4b72aa482761402f04c917d11deaa3964e3de462
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-09-12 14:24:13 -0300

    feat: add service to search credentials
    There is a service for multiple credentials from a given user and for one credential given a user and the credential id.xs

--- src/services/credentialService.ts
+++ src/services/credentialService.ts
@@ -7,7 +7,7 @@ import {
   CredentialCreationData
 } from '../types/credentialTypes';
 import { credentialRepository } from '../repositories';
-import { conflictException } from '../exceptions';
+import { conflictException, forbiddenException, notFoundException } from '../exceptions';
 
 dotenv.config();
 const secretKey: string = process.env.CRYPTR_SECRET || 'secret';
@@ -35,4 +35,30 @@ export async function makeNewCredential(
   delete createdCredential.password;
   const returningCredential: CredentialData = createdCredential;
   return returningCredential;
+}
+
+export async function retrieveUserCredentials(userId: number): Promise<Credential[]> {
+  const credentials: Credential[] = await credentialRepository.getUserCredentials(userId);
+  credentials.forEach((credential) => {
+    const password: string = credential.password;
+    const decriptedPassword: string = cryptr.decrypt(password);
+    credential.password = decriptedPassword;
+  });
+  return credentials;
+}
+
+export async function retrieveCredential(
+  credentialId: number,
+  requerentId: number
+): Promise<Credential> {
+  const credential: Credential | null = await credentialRepository
+    .getCredentialById(credentialId);
+  if (!credential) {
+    throw notFoundException('The credential you are trying to see doesn\'t exist');
+  }
+  if (credential.ownerId !== requerentId) {
+    throw forbiddenException('You are not the owner of this credential.');
+  }
+  credential.password = cryptr.decrypt(credential.password);
+  return credential;
 }
